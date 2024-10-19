179ec704b39a8130c57730ff59ec73c570eca789
DreamerHDR <107370928+DreamerHDR@users.noreply.github.com>
2024-10-19 14:58:31 +0300

    Add files via upload

---
+++ EncodingsApp/Lab9.1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Lab9._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxEncoding = new System.Windows.Forms.ComboBox();
+            this.textBoxContent = new System.Windows.Forms.TextBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxEncoding
+            // 
+            this.comboBoxEncoding.FormattingEnabled = true;
+            this.comboBoxEncoding.Location = new System.Drawing.Point(33, 24);
+            this.comboBoxEncoding.Name = "comboBoxEncoding";
+            this.comboBoxEncoding.Size = new System.Drawing.Size(193, 21);
+            this.comboBoxEncoding.TabIndex = 0;
+            // 
+            // textBoxContent
+            // 
+            this.textBoxContent.Location = new System.Drawing.Point(33, 65);
+            this.textBoxContent.Multiline = true;
+            this.textBoxContent.Name = "textBoxContent";
+            this.textBoxContent.Size = new System.Drawing.Size(405, 217);
+            this.textBoxContent.TabIndex = 1;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(33, 323);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(102, 47);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "Открыть";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(338, 323);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(100, 47);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 450);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.textBoxContent);
+            this.Controls.Add(this.comboBoxEncoding);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxEncoding;
+        private System.Windows.Forms.TextBox textBoxContent;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ EncodingsApp/Lab9.1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab9._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Добавляем популярные кодировки в ComboBox
+            comboBoxEncoding.Items.Add("UTF-8");
+            comboBoxEncoding.Items.Add("Windows-1251");
+            comboBoxEncoding.Items.Add("ISO-8859-1");
+            comboBoxEncoding.Items.Add("ASCII");
+
+            // Устанавливаем UTF-8 как кодировку по умолчанию
+            comboBoxEncoding.SelectedIndex = 0;
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Открываем диалог выбора файла
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        // Получаем выбранную кодировку из ComboBox
+                        string selectedEncoding = comboBoxEncoding.SelectedItem.ToString();
+                        Encoding encoding = Encoding.GetEncoding(selectedEncoding);
+
+                        // Читаем содержимое файла с выбранной кодировкой
+                        string content = File.ReadAllText(openFileDialog.FileName, encoding);
+
+                        // Отображаем содержимое в TextBox
+                        textBoxContent.Text = content;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка при открытии файла: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                // Открываем диалог сохранения файла
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        // Получаем выбранную кодировку из ComboBox
+                        string selectedEncoding = comboBoxEncoding.SelectedItem.ToString();
+                        Encoding encoding = Encoding.GetEncoding(selectedEncoding);
+
+                        // Сохраняем содержимое TextBox в файл с выбранной кодировкой
+                        File.WriteAllText(saveFileDialog.FileName, textBoxContent.Text, encoding);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка при сохранении файла: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
